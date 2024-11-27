ccceb0b9519feb664fbe054f40c14f5a80b9b42e
Federico-Zorzi <federico.zorzi05@gmail.com>
2024-11-27 16:56:19 +0100

    list filtered

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,50 @@
 import { useState } from "react";
+import { tasks } from "./data/data";
 import "./App.css";
 
 function App() {
-  /*   const [count, setCount] = useState(0)
+  const filteredTasks = (filters) =>
+    tasks.filter((task) => filters.includes(task.state));
+
+  const list = (filteredTasks) => {
+    return filteredTasks.map((task, index) => (
+      <li key={index} className="list-group-item">
+        <div className="title fw-semibold">
+          {task.title} <span>{task.state}</span>
+        </div>
+        <div>Priority {task.priority}</div>
+        <div>Est. time {task.estimatedTime}</div>
+      </li>
+    ));
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header>
+        <div className="container py-2">
+          <h1 className="fw-semibold">Task Manager</h1>
+        </div>
+      </header>
+
+      <div className="container py-2">
+        <h4 className="my-3 fw-semibold">
+          Current Tasks ({filteredTasks(["backlog", "in_progress"]).length})
+        </h4>
+
+        <ul className="list-group list-group-flush">
+          {list(filteredTasks(["backlog", "in_progress"]))}
+        </ul>
+
+        <hr />
+
+        <h4 className="my-3 fw-semibold">
+          Completed Tasks ({filteredTasks(["completed"]).length})
+        </h4>
+
+        <ul className="list-group list-group-flush">
+          {list(filteredTasks(["completed"]))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  ) */
-  return (
-    <>
-      <h1>My project</h1>
     </>
   );
 }
